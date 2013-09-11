d2090ef1f9696e1642d0e28ac4202676e1931f9e
Lukasz Wyszomirski <lwyszomirski@soldevelo.com>
2013-09-11 16:36:30 +0200

    added case list and simple details reports

--- corehq/apps/crs_reports/reports.py
+++ corehq/apps/crs_reports/reports.py
@@ -4,6 +4,8 @@ from corehq.apps.reports.standard import CustomProjectReport, DatespanMixin
 from corehq.apps.reports.fields import StrongFilterUsersField
 from corehq.apps.reports.datatables import DataTablesHeader, DataTablesColumn
 from corehq.apps.reports.standard.inspect import CaseDisplay, CaseListReport
+from django.utils import html
+from django.core.urlresolvers import reverse, NoReverseMatch
 
 import pytz
 from django.utils.translation import ugettext as _
@@ -16,28 +18,41 @@ class HNBCReportDisplay(CaseDisplay):
 
     @property
     def dob(self):
-        return self.parse_date(self.case['dob'])
+        return "" #self.parse_date(self.case['dob'])
 
     @property
     def visit_completion(self):
-        return ""
+        return "" #self.parse_date(self.case['visit_completion'])
 
     @property
     def delivery(self):
-        return ""
+        return "" #self.parse_date(self.case['delivery'])
 
     @property
     def pnc_status(self):
-        return ""
+        return "" #self.parse_date(self.case['pnc_status'])
+
+    @property
+    def case_link(self):
+        case_id, case_name = self.case['_id'], self.case['name']
+        try:
+            return html.mark_safe("<a class='ajax_dialog' href='%s'>%s</a>" % (
+                html.escape(reverse('case_details_report', args=[self.report.domain, case_id])),
+                html.escape(case_name),
+            ))
+        except NoReverseMatch:
+            return "%s (bad ID format)" % case_name
 
 
 class BaseHNBCReport(CustomProjectReport, DatespanMixin, CaseListReport):
+
     fields = ['corehq.apps.reports.fields.SelectBlockField',
               'corehq.apps.reports.fields.SelectSubCenterField',
               'corehq.apps.reports.fields.SelectASHAField',
               'corehq.apps.reports.fields.SelectPNCStatusField',
               'corehq.apps.reports.standard.inspect.CaseSearchFilter',
               'corehq.apps.reports.fields.DatespanField']
+
     ajax_pagination = True
     filter_users_field_class = StrongFilterUsersField
     include_inactive = True
@@ -71,6 +86,18 @@ class BaseHNBCReport(CustomProjectReport, DatespanMixin, CaseListReport):
                 disp.pnc_status,
             ]
 
+    # @property
+    # def case_filter(self):
+    #     filters = [{
+    #         'range': {
+    #             'opened_on': {
+    #                 "from": self.datespan.startdate_param_utc,
+    #                 "to": self.datespan.enddate_param_utc
+    #             }
+    #         }
+    #     }]
+    #     return filters
+
     @property
     @memoized
     def rendered_report_title(self):
@@ -87,6 +114,7 @@ class BaseHNBCReport(CustomProjectReport, DatespanMixin, CaseListReport):
 
 
 class HNBCMotherReport(BaseHNBCReport):
+
     name = ugettext_noop('Mother HNBC Form')
     slug = 'hnbc_mother_report'
 
--- corehq/apps/crs_reports/views.py
+++ corehq/apps/crs_reports/views.py
@@ -1,1 +1,14 @@
+from django.shortcuts import render
+from django.views.decorators.http import require_GET
+from corehq.apps.domain.decorators import login_and_domain_required
+from corehq.apps.reports.views import require_case_view_permission
 
+
+@require_case_view_permission
+@login_and_domain_required
+@require_GET
+def case_details_report(request, domain, case_id):
+     return render(request, 'crs_reports/mothers_form_reports_template.html', {
+        "domain": domain,
+        "case_id": case_id,
+    })
